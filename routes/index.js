340240144d67e0aa4ccbba04b3f38db250560533
Keith Moore <keith.m4@gmail.com>
2016-05-03 16:02:52 -0400

    added part of options page; and hashing for registration form

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var mongoUrl = 'mongodb://localhost:27017/coffee';
 var mongoose = require('mongoose');
 var Account = require('../models/accounts');
+var bcrypt = require('bcrypt-nodejs');
+//var session = require('express-session');
+
 mongoose.connect(mongoUrl);
 
 /* get route for the home page */
@@ -20,16 +23,24 @@ router.post('/register', function(req, res, next){
 	if (req.body.password !== req.body.password2){
 		res.redirect('/register?failure=password');
 	} else {
+		var salt = bcrypt.genSaltSync(10);
+
 		var newAccount = new Account({
 			username: req.body.username,
-			password: req.body.password,
+			password: bcrypt.hashSync(req.body.password, salt),
 			emailAddress: req.body.email
 		});
-
+		
 		console.log(newAccount);
 		newAccount.save();
-		res.json(req.body);
+		req.session.username = req.body.username;
+		res.redirect('/options');
 	}
+});
+
+/* get route for options page */
+router.get('/options', function(req, res, next){
+	res.render('options', { username: req.session.username });
 });
 
 module.exports = router;

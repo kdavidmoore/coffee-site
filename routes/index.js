888af1d2025a7b9b77712b1b9b5e864a9f073c4f
Keith Moore <keith.m4@gmail.com>
2016-05-03 12:26:12 -0400

    replaced pure.css with boostrap; added registration page; added section-background images back

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mongoUrl = 'mongodb://localhost:27017/coffee';
+var mongoose = require('mongoose');
+var Account = require('../models/accounts');
+mongoose.connect(mongoUrl);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DC Roasters' });
+/* get route for the home page */
+router.get('/', function(req, res, next){
+	res.render('index', {});
+});
+
+/* get route for register page */
+router.get('/register', function(req, res, next){
+	res.render('register', { failure: req.query.failure });
+});
+
+/* post route for register page */
+router.post('/register', function(req, res, next){
+	if (req.body.password !== req.body.password2){
+		res.redirect('/register?failure=password');
+	}
+
+	var newAccount = new Account({
+		username: req.body.username,
+		password: req.body.password,
+		emailAddress: req.body.email
+	});
+
+	console.log(newAccount);
+	newAccount.save();
+	res.json(req.body);
 });
 
 module.exports = router;
